cd8202f4a2468d7d54449067b4926af8dd069a1b
liyiyi <liyiyi@hexindai.com>
2015-12-31 14:35:53 +0800

    hello

---
+++ hello-mybatis/src/main/java/com/liyi/test/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package com.liyi.test.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.liyi.test.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ hello-mybatis/src/test/java/com/liyi/test/HelloWorldTest.java
@@ -0,0 +1,28 @@
+package com.liyi.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.liyi.test.dao.UserMapper;
+import com.liyi.test.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(locations="classpath*:applicationContext.xml")  
+public class HelloWorldTest {
+	@Resource
+	private UserMapper userMapper;
+	
+	  @Test
+	  public void testAddOpinion1() {
+		 User user =  userMapper.selectByPrimaryKey(1);
+		 System.out.println(user.getName());
+	  }
+
+}
